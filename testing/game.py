beec43206243258a2c41d1686b598f67071e86c2
Joel Figueroa <143023790+GunzRuls@users.noreply.github.com>
2025-11-12 14:22:13 -0500

    Add player input logic

--- testing/game.py
+++ testing/game.py
@@ -1,1 +1,4 @@
-print("Welcome to Rock-Paper-Scissors")
+print("Welcome to Rock-Paper-Scissors!")
+player_input = ''
+while player_input not in ['rock','paper','scissors']:
+    player_input = input("Choose rock, paper, or scissors: ").lower()
