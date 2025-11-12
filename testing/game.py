3cdf38566a31235a6f0818fe50398a5e91198360
Joel Figueroa <143023790+GunzRuls@users.noreply.github.com>
2025-11-12 14:25:11 -0500

    person 1 done

--- testing/game.py
+++ testing/game.py
@@ -2,3 +2,4 @@ print("Welcome to Rock-Paper-Scissors!")
 player_input = ''
 while player_input not in ['rock','paper','scissors']:
     player_input = input("Choose rock, paper, or scissors: ").lower()
+print(f"You chose: {player_input}")
